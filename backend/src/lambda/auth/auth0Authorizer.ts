1945efca53dc74db116d2b5836060dfe40b75236
sjaschke <sascha@jaschke.it>
2020-06-06 20:43:35 +0200

    gitflow-feature-stash: implemnt_auth

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,18 +1,19 @@
-import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
+import {CustomAuthorizerEvent, CustomAuthorizerResult} from 'aws-lambda'
 import 'source-map-support/register'
 
-import { verify, decode } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger'
-import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
-import { JwtPayload } from '../../auth/JwtPayload'
+import {decode, verify} from 'jsonwebtoken'
+import {createLogger} from '../../utils/logger'
+import {Jwt} from '../../auth/Jwt'
+import {JwtPayload} from '../../auth/JwtPayload'
+
+const fetch = require('node-fetch');
 
 const logger = createLogger('auth')
 
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl = 'https://dev-8pv1ga6i.eu.auth0.com/.well-known/jwks.json';
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -58,10 +59,20 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  // TODO: Implement token verification
-  // You should implement it similarly to how it was implemented for the exercise for the lesson 5
-  // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  let response = await fetch(jwksUrl).json();
+  let data = await response.json();
+
+  const cert = data.keys
+      .filter(key => key.use === 'sig' // JWK property `use` determines the JWK is for signing
+          && key.kty === 'RSA' // We are only supporting RSA (RS256)
+          && key.kid           // The `kid` must be present to be useful for later
+          && ((key.x5c && key.x5c.length) || (key.n && key.e)) // Has useful public keys
+      ).map(key => {
+        return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) };
+      }).find(key => key.kid === jwt.header.kid);
+
+  let verifiedToken = verify(token, cert, { algorithms: ['RS256'] }) as Jwt;
+  return verifiedToken.payload;
 }
 
 function getToken(authHeader: string): string {
@@ -71,7 +82,11 @@ function getToken(authHeader: string): string {
     throw new Error('Invalid authentication header')
 
   const split = authHeader.split(' ')
-  const token = split[1]
+  return split[1]
+}
 
-  return token
+function certToPEM(cert) :string{
+  cert = cert.match(/.{1,64}/g).join('\n');
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+  return cert;
 }
